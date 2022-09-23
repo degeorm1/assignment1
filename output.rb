29660b32a0f19ef713dd0e7d9dccc31febcfaa0c
Michael De George <112439978+degeorm1@users.noreply.github.com>
2022-09-22 21:09:34 -0400

    Create output.rb

---
+++ output.rb
@@ -0,0 +1,17 @@
+=begin 
+Michael De George
+csc415
+9/22/22
+=end
+
+#include all the other files created for the assignemtn
+
+require_relative 'QuickGroups'
+require_relative 'sort'
+require_relative 'VE'
+
+
+
+
+File.open("output.csv", "w+") { |file| file.write("QuicK Groups Array") } #Creates Output file in CSV format
+
